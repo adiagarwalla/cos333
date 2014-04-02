2745cb145a07b79b06b247a9dbe0750966002f18
Helen Yu <heleny@princeton.edu>
2014-04-01 23:49:02 -0400

    Hopefully this restores it back to normal

---
+++ Qurious_iOS/Qurious_iOS/EditViewController.h
@@ -0,0 +1,19 @@
+//
+//  EditViewController.h
+//  Qurious_iOS
+//
+//  Created by Helen Yu on 3/31/14.
+//  Copyright (c) 2014 Qurious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UITableViewController <UITextFieldDelegate>
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextView *bioField;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ Qurious_iOS/Qurious_iOS/MasterViewController.m
@@ -0,0 +1,117 @@
+//
+//  MasterViewController.m
+//  Qurious_iOS
+//
+//  Created by Aditya Agarwalla on 3/31/14.
+//  Copyright (c) 2014 Qurious. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "Person.h"
+#import "DetailViewController.h"
+
+@interface MasterViewController () {
+    NSMutableArray *_objects;
+}
+@end
+
+@implementation MasterViewController
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    self.navigationItem.rightBarButtonItem = addButton;
+}
+
+// used to reload the list, unfortunately it crashes
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    UITableView *view = (UITableView *)self.view;
+    [view reloadData];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)insertNewObject:(id)sender
+{
+    if (!_objects) {
+        _objects = [[NSMutableArray alloc] init];
+    }
+    Person *friend = [[Person alloc] init];
+    friend.firstName = @"Aditya";
+    friend.lastName = @"Agarwalla";
+    friend.email = @"aa4@princeton.edu";
+    friend.bio = @"Hi, I'm Aditya! I like to code! I am a student at Princeton University. I would like to learn how to fix a bike and cook Chinese food. Hi, I'm Aditya! I like to code! I am a student at Princeton University. I would like to learn how to fix a bike and cook Chinese food.";
+    NSMutableArray *skills = [NSMutableArray arrayWithObjects:@"Coding in Java", @"Coding in Python", @"Eating", @"Sleeping", @"Pooping", @"Going to class", nil];
+    friend.skills = skills;
+    UIImage *profPic = [UIImage imageNamed:@"dogesmall.jpg"];
+    friend.profPic = profPic;
+    [_objects insertObject:friend atIndex:0];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath]
+                          withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _objects.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView
+                             dequeueReusableCellWithIdentifier:@"Cell"
+                             forIndexPath:indexPath];
+    Person *friend = _objects[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@",
+                           friend.firstName, friend.lastName];
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [_objects removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+}
+
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        Person *friend = _objects[indexPath.row];
+        [[segue destinationViewController] setDetailItem:friend];
+    }
+}
+
+@end
