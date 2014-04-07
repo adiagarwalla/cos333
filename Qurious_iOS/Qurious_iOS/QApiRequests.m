eb10712724ed1ca404bd2bce182d7a8ac939c9a0
Abhinav Khanna <akhanna@princeton.edu>
2014-04-07 19:59:31 -0400

    updated to add the endpoint in xcode for the all profiles

--- Qurious_iOS/Qurious_iOS/QApiRequests.m
+++ Qurious_iOS/Qurious_iOS/QApiRequests.m
@@ -16,19 +16,19 @@ char* baseURL = "http://localhost:8000";
 
 + (void) getProfiles:(int)user_id andCallback:(void(*)(id))callback {
     AsyncRequest* request = [AsyncRequest new];
-    NSString* url = [NSString stringWithFormat:@"%s/api/profile?id=%d", baseURL, user_id];
+    NSString* url = [NSString stringWithFormat:@"%s/api-profile/profile?id=%d", baseURL, user_id];
     [request startAsync:callback andUrl:url];
 }
 
 + (void) getSkillDetails:(int)skill_id andCallback:(void(*)(id))callback {
     AsyncRequest* request = [AsyncRequest new];
-    NSString* url = [NSString stringWithFormat:@"%s/api/skills?id=%d", baseURL, skill_id];
+    NSString* url = [NSString stringWithFormat:@"%s/api-profile/skills?id=%d", baseURL, skill_id];
     [request startAsync:callback andUrl:url];
 }
 
 + (void) editProfile:(NSString*)profile_name andBio:(NSString*)bio andEmail:(NSString*)email andCallback:(void(*)(id))callback {
     AsyncRequest* request = [AsyncRequest new];
-    NSString* url = [NSString stringWithFormat:@"%s/api/profile", baseURL];
+    NSString* url = [NSString stringWithFormat:@"%s/api-profile/profile", baseURL];
     NSDictionary* dict = [[NSDictionary alloc] initWithObjectsAndKeys:profile_name,@"profile_name", bio, @"user_bio", email, @"user_email", nil];
     [request startAsyncPost:callback andUrl:url andDict:dict];
 }
@@ -36,9 +36,15 @@ char* baseURL = "http://localhost:8000";
 + (void) editSkill:(int)skill_id andPrice:(NSString*)price andDesc:(NSString*)desc andForSale:(BOOL)isMarketable andCallback:(void(*)(id))callback {
     
     AsyncRequest* request = [AsyncRequest new];
-    NSString* url = [NSString stringWithFormat:@"%s/api/skills?skill_id=%d", baseURL, skill_id];
+    NSString* url = [NSString stringWithFormat:@"%s/api-profile/skills?skill_id=%d", baseURL, skill_id];
     NSDictionary* dict = [[NSDictionary alloc] initWithObjectsAndKeys:price,@"price", desc, @"desc", isMarketable, @"marketable", nil];
     [request startAsyncPost:callback andUrl:url andDict:dict];
+}
+
++ (void) getAllProfiles:(void(*)(id))callback {
+    AsyncRequest* request = [AsyncRequest new];
+    NSString* url = [NSString stringWithFormat:@"%s/api-profile/allprofile", baseURL];
+    [request startAsync:callback andUrl:url];
 }
 
 @end
