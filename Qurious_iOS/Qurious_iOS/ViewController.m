32910a93a8bbf6fb5a215b21f4845ea0835f5c24
adiagarwalla <aa4@princeton.edu>
2014-04-09 21:37:25 -0400

    Some setup stuff for opentok

---
+++ Qurious_iOS/Qurious_iOS/ViewController.m
@@ -0,0 +1,13 @@
+//
+//  ViewController.m
+//  Qurious_iOS
+//
+//  Created by Aditya Agarwalla on 4/9/14.
+//  Copyright (c) 2014 Qurious. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+@end
