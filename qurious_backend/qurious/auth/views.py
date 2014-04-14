74a1e1ddeaa84dadd9a2b2b642a5d9ff0ced7435
Jon <jkwok@princeton.edu>
2014-04-13 21:14:18 -0400

    signup changes

--- qurious_backend/qurious/auth/views.py
+++ qurious_backend/qurious/auth/views.py
@@ -39,17 +39,21 @@ class QuriousSignUpView(View):
         form = ProfileSignUpForm(request.POST)
 
         if form.is_valid():
-            user_dummy = User(username=form.cleaned_data.get('username'), email=form.cleaned_data.get('user_email'))
-            user_dummy.set_password(form.cleaned_data.get('password'))
-            user_dummy.save()
-            user_dummy_profile = UserProfile(user=user_dummy, profile_name=user_dummy.username, user_email=user_dummy.email, user_bio='')
+            try:
+                user = User.objects.get(username=form.cleaned_data.get('username'))
+                return HttpResponse('', mimetype='application/json')
+            except:
+                user_dummy = User(username=form.cleaned_data.get('username'), email=form.cleaned_data.get('user_email'))
+                user_dummy.set_password(form.cleaned_data.get('password'))
+                user_dummy.save()
+                user_dummy_profile = UserProfile(user=user_dummy, profile_name=user_dummy.username, user_email=user_dummy.email, user_bio='')
             
-            s = Skill(name='learning', desc='I like to learn!', price=0, is_marketable=False)
-            user_dummy_profile.save()
-            s.save()
-            user_dummy_profile.skills.add(s)
-            user_dummy_profile.save()
+                s = Skill(name='learning', desc='I like to learn!', price=0, is_marketable=False)
+                user_dummy_profile.save()
+                s.save()
+                user_dummy_profile.skills.add(s)
+                user_dummy_profile.save()
             
-            return HttpResponse('1', mimetype='application/json')
+                return HttpResponse('1', mimetype='application/json')
         else:
             return HttpResponse('', mimetype='application/json')
