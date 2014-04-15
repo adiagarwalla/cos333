513664a637118b0aaff5af281ccb30247bfe7b37
Helen Yu <heleny@princeton.edu>
2014-04-14 22:32:42 -0400

    prelim work on profiles

--- Qurious_iOS/Qurious_iOS/ProfileViewController.h
+++ Qurious_iOS/Qurious_iOS/ProfileViewController.h
@@ -1,15 +1,14 @@
 //
-//  DetailViewController.h
+//  ProfileViewController.h
 //  Qurious_iOS
 //
-//  Created by Aditya Agarwalla on 3/31/14.
+//  Created by Helen Yu on 4/14/14.
 //  Copyright (c) 2014 Qurious. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DetailViewController : UITableViewController
-
+@interface ProfileViewController : UITableViewController
 @property (strong, nonatomic) id detailItem;
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *emailLabel;
@@ -17,4 +16,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)save:(UIStoryboardSegue *)sender;
+- (IBAction)cancel:(UIStoryboardSegue *)sender;
 @end
+
