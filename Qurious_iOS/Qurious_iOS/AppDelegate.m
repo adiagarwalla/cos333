3f5c1757a71ab0b10e97281d79b64fbad1c4b912
adiagarwalla <aa4@princeton.edu>
2014-04-14 23:23:47 -0400

    Files setup for panel

--- Qurious_iOS/Qurious_iOS/AppDelegate.m
+++ Qurious_iOS/Qurious_iOS/AppDelegate.m
@@ -8,16 +8,17 @@
 
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-//    
 //    self.window.rootViewController = [[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil];
-//    
-//    [self.window makeKeyAndVisible];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.rootViewController = [[MainViewController alloc] initWithNibName:@"MainViewController" bundle:nil];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ Qurious_iOS/Qurious_iOS/CenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CenterViewController.h
+//  Qurious_iOS
+//
+//  Created by Aditya Agarwalla on 4/14/14.
+//  Copyright (c) 2014 Qurious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenterViewController : UIViewController
+
+@end
