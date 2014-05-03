1b51a2cc7e7aee49864b612457ecab60f256911b
adiagarwalla <aa4@princeton.edu>
2014-05-03 18:03:44 -0400

    Navbar titles all uniform and with style changes

--- Qurious_iOS/Qurious_iOS/AppDelegate.m
+++ Qurious_iOS/Qurious_iOS/AppDelegate.m
@@ -18,6 +18,12 @@
 {
     UIImage *navBackgroundImage = [UIImage imageNamed:@"linen3.jpg"];
     [[UINavigationBar appearance] setBackgroundImage:navBackgroundImage forBarMetrics:UIBarMetricsDefault];
+    NSShadow *shadow = [[NSShadow alloc] init];
+    shadow.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8];
+    shadow.shadowOffset = CGSizeMake(0, 1);
+    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
+                                                           [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
+                                                           shadow, NSShadowAttributeName, nil]];
 //    self.window.rootViewController = [[LoginViewController alloc] initWithNibName:@"LoginViewController" bundle:nil];
 //    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
