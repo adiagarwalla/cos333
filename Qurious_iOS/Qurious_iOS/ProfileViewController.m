0a789f58c888ea72710ab18d49ccffacffb0fdad
Helen Yu <heleny@princeton.edu>
2014-05-06 21:21:35 -0400

    back button on edit skills now working

--- Qurious_iOS/Qurious_iOS/ProfileViewController.m
+++ Qurious_iOS/Qurious_iOS/ProfileViewController.m
@@ -54,9 +54,10 @@ static ProfileViewController* _self;
         [editViewController setDetailItem:me];
     }
     if ([[segue identifier] isEqualToString:@"showSkillEdit"]) {
-        NSArray *navigationControllers = [[segue destinationViewController] viewControllers];
-        SkillViewController *skillViewController = [navigationControllers objectAtIndex:0];
-        [skillViewController setDetailItem:me];
+        [[segue destinationViewController] setDetailItem:me];
+//        NSArray *navigationControllers = [[segue destinationViewController] viewControllers];
+//        SkillViewController *skillViewController = [navigationControllers objectAtIndex:0];
+//        [skillViewController setDetailItem:me];
     }
     
 }
